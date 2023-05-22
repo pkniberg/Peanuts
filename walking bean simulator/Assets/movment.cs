6e7d2cc681c3d9cc68d1f0763294b7133e283442
pkniberg <peter@kniberg.com>
2023-05-22 17:20:54 +0200

    Error = Gone
    Yes, very nice!

--- walking bean simulator/Assets/movment.cs
+++ walking bean simulator/Assets/movment.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class PlayerMovementTutorial : MonoBehaviour
+public class movment : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed;
