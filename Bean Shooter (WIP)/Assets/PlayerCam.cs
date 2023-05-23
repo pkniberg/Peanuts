bb4e2e78481126c7534daf603060d7a4926e6c4f
pkniberg <peter@kniberg.com>
2023-05-23 18:01:09 +0200

    Tried to add rotating camera... but failed :(
    Some bugs with "Move Camera" and "Player Cam"
    
    (they are both scripts)

---
+++ Bean Shooter (WIP)/Assets/PlayerCam.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+    public Transform orientation;
+    float xRotation;
+    float yRotation;
+    
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("mouse X") *  Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("mouse Y") *  Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+
+        xRotation += mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation =  Quaternion.Euler(0, yRotation, 0);
+    }
+}
